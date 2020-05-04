2309a591234b0361bb61b0ca97b3fc3fca58fb90
matthew230506 <56759474+matthew230506@users.noreply.github.com>
2020-05-04 14:41:57 +0200

    schule added channel message

--- commands/schule.js
+++ commands/schule.js
@@ -10,7 +10,9 @@ class schule {
 	isThisCommand(command) {
 		return command === this._command;
 	}
-	runCommand(args, message, client) {}
+	runCommand(args, message, client) {
+		message.channel.send(`<#688764704054575204>`);
+	}
 }
 exports.default = schule;
 //# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYmFuLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiLi4vLi4vc3JjL2NvbW1hbmRzL2Jhbi50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOztBQUdBLE1BQXFCLEdBQUc7SUFBeEI7UUFDa0IsYUFBUSxHQUFHLEtBQUssQ0FBQztJQVduQyxDQUFDO0lBVEEsSUFBSTtRQUNILE9BQU8sa0JBQWtCLENBQUM7SUFDM0IsQ0FBQztJQUNELGFBQWEsQ0FBQyxPQUFlO1FBQzVCLE9BQU8sT0FBTyxLQUFLLElBQUksQ0FBQyxRQUFRLENBQUM7SUFDbEMsQ0FBQztJQUNELFVBQVUsQ0FBQyxJQUFjLEVBQUUsT0FBd0IsRUFBRSxNQUFzQjtJQUUzRSxDQUFDO0NBQ0Q7QUFaRCxzQkFZQyJ9
